e63213a784212fe9253fb2601376eb611ac9df43
Phoenix7290 <b5261064@gmail.com>
2025-08-02 16:28:26 -0300

    Small fixes in TP1

--- tp1/ex10/main.py
+++ tp1/ex10/main.py
@@ -4,5 +4,8 @@ def main():
     print(f'Caracter A: {opiniao.count('a')}') # count, diferente do dito no enunciado, conta as ocorrencias de determinada string
     print(f'Palavras na Frase: {len(opiniao.split())}') # seria mais correto para contas as palavras
     
+    # Há uma forma que é possível fazer funcionar o count com:
+    print(f'Palavras na Frase: {opiniao.count('a') +1}')
+    
 if __name__ == "__main__":
     main()
